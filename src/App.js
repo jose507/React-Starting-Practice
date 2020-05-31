9d2ef3a156c37f120a094c9cafd8b1733c078277
jose507 <jlrmy@gmail.com>
2020-05-31 13:35:00 -0500

    moving foward into the react components theory

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import Person from './Person/Person';
 class App extends Component {
   render() {
      return (
       <div className="App">
         <h1>Hi I´m a react App</h1>
         <p>This is really working!</p>
+        <Person />
       </div>
     );
     //return React.createElement('div',{className: 'App'}, React.createElement('h1',null, 'Does this  work for now'));
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,1 +1,7 @@
-const person
+import React from 'react';
+
+const person = () =>{
+return <p>Im a person I am {Math.floor(Math.random()*30)} years old!</p>
+};
+ 
+export default person;
